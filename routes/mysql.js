c5cf59a18395c42022af6cf20d3c22f2c28c1dde
zdm <zdm@test.com>
2024-08-30 21:33:58 +0800

    fix

--- routes/mysql.js
+++ routes/mysql.js
@@ -1106,7 +1106,7 @@ exports.addBatchAuthCodes = function addBatchAuthCodes(authCodes, userName) {
             } else {
                 var sql = 'insert into authentic_code_tbl (authenticCode, productInfo, creator, updator) values ?';
                 var sqlParams = authCodes.map(code => [code.authenticCode, code.productInfo, userName, userName]);
-                conn.query(sql, sqlParams, function (err) {
+                conn.query(sql, [sqlParams], function (err) {
                     conn.release();
                     if (err) {
                         reject(err);
